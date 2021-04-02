aeb6a35f159f0f3a25e4664167134d76ad035948
Anggitri Sihombing <anggitrisihombing@gmail.com>
2021-04-02 18:04:43 +0700

    menambah fungsi

---
+++ tampilTabel.php
@@ -0,0 +1,46 @@
+<?php 
+require 'functions.php';
+?>
+
+<!DOCTYPE html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta nam e="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Pasien</title>
+</head>
+<body>
+
+<h1>Daftar Pasien </>
+
+<table border="1" cellspacing="0">
+    <tr>
+        <th> No. </th>
+        <th> No Rekam Medis </th>
+        <th> Nama </th>
+        <th> Jenis Kelamin </th>
+        <th> Umur </umur>
+        <th> No Handphone </th>
+        <th> Poli </th>
+        <th> Alamat </th>
+    </tr>
+
+    <tr>
+        <td> 1 </td>
+        <td>
+            <a href="">edit</a> |
+            <a href="">hapus</a>
+        </td>
+        <td> Nama </td>
+        <td> Jenis Kelamin </td>
+        <td> Umur </umur>
+        <td> No Handphone </td>
+        <td> Poli </td>
+        <td> Alamat </td>
+    </tr>
+
+</table>
+
+    
+</body>
+</html>
